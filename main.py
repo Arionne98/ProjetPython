234f9c1ecb3968656b0c82e9fb54062921c68af5
Arionne <aoron974@gmail.com>
2023-03-01 11:44:28 +0400

    Version 0.0.1

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+
+class Game:
+
+    def __init__(self, title, resolution):
+        self.title = title
+        self.resolution = resolution 
+
+    def charger_module(self):
+        pygame.init()
+
+    def display(self):
+        pygame.display.set_caption(self.title)
+        display = pygame.display.set_mode(self.resolution)
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+minecraft = Game("Minecraft 1.7.10", (600, 500))
+
+minecraft.charger_module()
+minecraft.display()
+minecraft.run()
+
